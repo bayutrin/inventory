a8302f97e0c5e305a58d4fe0ba58503dce5f6f58
bayutrin <bayutrin95@gmail.com>
2016-11-29 14:03:54 +0700

    14.03 - 291116 menambahkan file header.php dan headerTemplate.php

---
+++ inventory/views/barang/templates/headerTemplate.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  	<head>
+		<title>daily kki</title>
+	   <link href="<?php echo base_url() ?>template/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+      <link href="<?php echo base_url() ?>template/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+      <link href="<?php echo base_url() ?>template/assets/styles.css" rel="stylesheet" media="screen">
+      <link href="<?php echo base_url() ?>template/assets/DT_bootstrap.css" rel="stylesheet" media="screen">
+      <script src="vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+   </head>
+
